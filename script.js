f68d30e1f0006e152bd41ea08c8d54a4e46d664a
gilestodd166 <167215806+GilesTodd166@users.noreply.github.com>
2025-10-29 14:40:45 -0400

    Completed Library App.
    Added delete functionality and read status issues
    solved.

--- script.js
+++ script.js
@@ -1,35 +1,51 @@
+window.onload = function() {
+    displayLibrary();
+}
+
 const cardContainer = document.getElementById('cards-container');
 
 const dialog = document.querySelector('dialog');
-const showButton = document.querySelector('button');
+const showButton = document.getElementById('addBook');
 const closeButton = document.getElementById('close');
 const submitButton = document.getElementById('submit');
 const myForm = document.getElementById('bookForm');
 
-const myLibrary = [
+let myLibrary = [
     {
-        "title": "The Hobbit",
-        "author": "J.R.R. Tolkein",
-        "pages": 956,
-        "genre": "Fantasy",
-        "read": "Has read"
+        title: "The Hobbit",
+        author: "J.R.R. Tolkein",
+        pages: 956,
+        genre: "Fantasy",
+        read: "Has read",
+        id: crypto.randomUUID()
+    },
+    {
+        title: "Greenwood",
+        author: "Michael Christie",
+        pages: 617,
+        genre: "Sci-Fi",
+        read: "Has not read",
+        id: crypto.randomUUID()
     }
 ];
-
-// addBookToLibrary('The Hobbit', 'J.R.R. Tolkein', 892, 'Fantasy', true);
-// addBookToLibrary('Greenwood', 'Michael Christie', 654, 'Sci-Fi', true);
-// addBookToLibrary('Braiding Sweetgrass', 'Robin Wall Kimmer', 531, 'Non-Fiction', false);
-// addBookToLibrary('The Wall', 'Marlin Haushofer', 1003, 'Sci-Fi', false);
 
 function Book(title, author, pages, genre, read) {
     if (!new.target) {
         throw Error('This is an error creating a new book')
-    }
+    };
+
+    let readStatus;
+        if (read === "true") {
+            readStatus = "Has read";
+        } else {
+            readStatus = "Has not read"
+        };
+
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.genre = genre;
-    this.read = read ? "Has read" : "Has not read";
+    this.read = readStatus;
     this.id = crypto.randomUUID();
 };
 
@@ -39,40 +55,60 @@ function addBookToLibrary(title, author, pages, genre, read, id) {
     displayLibrary();
 };
 
-// Loop myLibrary and update DOM.
+// Loop through myLibrary and display book entires.
 function displayLibrary() {
     cardContainer.innerHTML = '';
-    myLibrary.forEach((book) => {
+        myLibrary.forEach((book) => {
 
-    const newCard = document.createElement('div');
-    newCard.setAttribute('id', 'book-card');
-    cardContainer.append(newCard);
+        const newCard = document.createElement('div');
+            newCard.setAttribute('id', 'book-card');
+            cardContainer.append(newCard);
 
-    const title = document.createElement('div');
-    title.setAttribute('id', 'title');
-    title.textContent = `Title: ${book.title}`;
-    newCard.append(title);
+        const title = document.createElement('div');
+            title.setAttribute('id', 'title');
+            title.textContent = `Title: ${book.title}`;
+            newCard.append(title);
 
-    const author = document.createElement('div');
-    author.setAttribute('id', 'author');
-    author.textContent = `Author: ${book.author}`;
-    newCard.append(author);
+        const author = document.createElement('div');
+            author.setAttribute('id', 'author');
+            author.textContent = `Author: ${book.author}`;
+            newCard.append(author);
 
-    const pages = document.createElement('div');
-    pages.setAttribute('id', 'pages');
-    pages.textContent = `Pages: ${book.pages}`;
-    newCard.append(pages);
+        const pages = document.createElement('div');
+            pages.setAttribute('id', 'pages');
+            pages.textContent = `Pages: ${book.pages}`;
+            newCard.append(pages);
 
-    const genre = document.createElement('div');
-    genre.setAttribute('id', 'genre');
-    genre.textContent = `Genre: ${book.genre}`;
-    newCard.append(genre);
+        const genre = document.createElement('div');
+            genre.setAttribute('id', 'genre');
+            genre.textContent = `Genre: ${book.genre}`;
+            newCard.append(genre);
 
-    const read = document.createElement('div');
-    read.setAttribute('id', 'read');
-    read.textContent = `Read: ${book.read}`;
-    newCard.append(read);
+        const read = document.createElement('div');
+            read.setAttribute('id', 'read');
+            read.textContent = `Read: ${book.read}`;
+            
+            newCard.append(read);
 
+        // Delete Book Button
+        const delDiv = document.createElement('div');
+            delDiv.setAttribute('id', 'delButton');
+            newCard.append(delDiv);
+
+        const delButton = document.createElement('button');
+            delButton.setAttribute('id', 'del-button');
+            delButton.innerHTML = `Delete Book`;
+            delDiv.append(delButton);
+
+            let id = book.id;
+
+            // Delete button functionality
+            delButton.addEventListener("click", () => {
+                const bookID = book.id;
+                myLibrary = myLibrary.filter(item => item.id !== bookID);
+
+            displayLibrary();
+            });
     });
 };
 
@@ -84,14 +120,15 @@ showButton.addEventListener("click", () => {
 closeButton.addEventListener("click", (e) => {
     e.preventDefault();
     dialog.close();
+    
 });
 
 // Form submit and Library Update
-bookForm.addEventListener("submit", (e) => {
+bookForm.addEventListener("submit", (event) => {
 
-    e.preventDefault(); 
+    event.preventDefault(); 
 
-        console.log('Form submitted!');
+        // console.log('Form submitted!');
 
         let title = bookForm.elements.title.value; 
         let author = bookForm.elements.author.value;
